e835617948ba38a8568017d8311167904351bc7d
tellinwood <57632041+tellinwood@users.noreply.github.com>
2019-11-11 10:40:11 -0500

    Add files via upload

--- CompassTateRCTest/CompassTateRCTest.ino
+++ CompassTateRCTest/CompassTateRCTest.ino
@@ -10,21 +10,33 @@ int targetHeading;              // where we want to go to reach current waypoint
 int currentHeading;             // where we are actually facing now
 int headingError;               // signed (+/-) difference between targetHeading and currentHeading
 float headingDegrees;
-#define HEADING_TOLERANCE 5     // tolerance +/- (in degrees) within which we don't attempt to turn to intercept targetHeading
+int newDirection; // desired direction sent via ROS
+int newDirectionUpperLimit; // upper tolerance for degrees (TateRC will not try to adjust to exactly newDirection, but instead to within these limits)
+int newDirectionLowerLimit; // lower tolerance for degrees
+int newDirectionTolerance; // tolerance, which can be set globally (degree span between newDirectionUpperLimit and newDirectionLowerLimit)
+
 
 void setup() {
   // put your setup code here, to run once:
   mag.begin();
   Serial.begin(9600);
   Serial.println("Compass heading");
+  newDirectionTolerance = 10
+  
 }
+
+void compassAllignMsg( const std_msgs::Int8& ROSNewDirection)
+{
+  newDirection = ROSNewDirection.data;
+  compassAllign(newDirection);
+}
+
+ros::Subscriber<std_msgs::Int8> sub1("compassAllign", &compassAllignMsg ); //<std_msgs::Int8> defines datatype, you need to include each datatype library you use globally (very top of this tab).
 
 void loop() {
 
   currentHeading = readCompass();
   Serial.println(headingDegrees); //this is the current heading (in degrees) of TateRC. Use this to create an autonomous function that compares it to an input direction
-  delay(10);
-  compassAllign(headingDegrees);
 }
 
 
@@ -34,7 +46,7 @@ int readCompass(void)
   mag.getEvent(&event);
   float heading = atan2(event.magnetic.y, event.magnetic.x);
 
-  float headingDegrees = heading * 180 / M_PI;
+  float headingDegrees = heading * 180 / M_PI; //headingDegrees is the current heading of TateRC
 
   return ((int)headingDegrees);
 }
--- CompassTateRCTest/functions.ino
+++ CompassTateRCTest/functions.ino
@@ -1,4 +1,18 @@
-void compassAllign(float headingDegrees)
+void compassAllign(int newDirection)
 {
-  
+  Status_Update("new direction received");
+
+  newDirectionLowerLimit = (newDirection - (newDirectionTolerance / 2);
+  newDirectionUpperLimit = (newDirection + (newDirectionTolerance / 2);
+
+  if (headingDegrees < newDirectionLowerLimit && headingDegrees > newDirectionUpperLimit)
+  {
+    Status_Update("gimme a sec")
+     //drive in a circle (pseudo code)
+  }
+  else if (headingDegrees > newDirectionLowerLimit && headingDegrees < newDirectionUpperLimit)
+  {
+    Status_Update("arrived at new heading");
+    //stop motors (whatever drive and servo RELEASE) fix this pseudo code
+  }
 }
